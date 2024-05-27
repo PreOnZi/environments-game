c6e689f8b5c34c5bef12303295785a0e5d67cd09
Ondrej Zika <o.zika0120211@arts.ac.uk>
2024-05-27 16:23:25 +0100

    updated main page game name

--- sketch_may13a/sketch_may13a.ino
+++ sketch_may13a/sketch_may13a.ino
@@ -1,63 +1,47 @@
 #include <Servo.h>
 
-// Define the pins for the servo
+
 const int servoPin = 9;
 
 Servo myServo;
-int originalPosition = 1500; // Original position of the servo (1500 microseconds)
+int originalPosition = 1500; // Original position
 
 void setup() {
-  // Initialize serial communication for debugging
-  //Serial.begin(115200);
+
 Serial.begin(9600);
-  // Attach the servo to its pin
+ 
   myServo.attach(servoPin);
 
-  // Set the initial position of the servo to the center (1500 microseconds)
+  // Intial Position (1500 microsec.)
   myServo.writeMicroseconds(originalPosition);
 }
 
 void loop() {
-  // Reset the servo to the original position at the beginning of each loop iteration
+  // servo to original position after each loop
   myServo.writeMicroseconds(originalPosition);
 
-  // Read serial input:
   while (Serial.available() > 0) {
-    // Read the incoming value
     int pulseWidth = Serial.parseInt();
-    
-    // Ensure the pulse width is within valid range (500 to 2500 microseconds)
     pulseWidth = constrain(pulseWidth, 500, 2500);
     
-    // Store the original position before moving the servo
+    // store the original position
     originalPosition = myServo.readMicroseconds();
-
-    // Set the servo position based on the received pulse width
+    // set the servo position based on pulse
     myServo.writeMicroseconds(pulseWidth);
 
-    // Print the pulse width for debugging
-    Serial.print("Pulse width set to: ");
-    Serial.println(pulseWidth);
+    delay(1000);
 
-    // Delay for a moment (adjust as needed)
-    delay(1000); // Delay for 1 second (1000 milliseconds)
-
-    // Calculate the reverse position
+    // reverse calculation
     int reversePosition = originalPosition - (pulseWidth - originalPosition);
-
-    // Set the servo to the reverse position
     myServo.writeMicroseconds(reversePosition);
 
-    // Delay for a moment (adjust as needed)
-    delay(1000); // Delay for 1 second (1000 milliseconds)
+    delay(1000);
 
-    // Return the servo to the original position
+    // servo to original pos
     myServo.writeMicroseconds(originalPosition);
 
-    // Delay for a moment (adjust as needed)
-    delay(1000); // Delay for 1 second (1000 milliseconds)
+    delay(1000);
 
-    // Clear the serial input buffer before reading new data
     while (Serial.available() > 0) {
       Serial.read();
     }
